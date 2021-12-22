a1d0080041adc4cc104e2d2aa0e1dbfdcd030aec
j k <jakob.kralj04@gmail.com>
2021-12-22 20:22:34 +0100

    day21 done

--- Day16/task1.jl
+++ Day16/task1.jl
@@ -1,9 +1,15 @@
 function literalValue(raw)
-    version = parse(Int64, raw[1:3])
-    type = parse(Int64, raw[4:6])
+    if length(raw) < (7 + 4)
+        return -1
+    end
+    version = parse(Int64, raw[1:3], base = 2)
+    type = parse(Int64, raw[4:6], base = 2)
     i = 7
     nmstr = ""
     while true
+        if length(raw) < i + 4
+            return -1
+        end
         nmstr *= raw[i+1:i+4]
         if raw[i] == '1'
             i += 5
@@ -12,15 +18,117 @@ function literalValue(raw)
         end
     end
     i += 5
-    return ((version, type, parse(Int, nmstr), base=2), i)
+    return ((version, type, parse(Int64, nmstr, base = 2)), i)
+end
+function Operators(raw)
+    version = parse(Int64, raw[1:3], base = 2)
+    type = parse(Int64, raw[4:6], base = 2)
+    id = parse(Int64, raw[7], base = 2)
+    res = []
+    ind = 0
+    if id == 0
+        len = parse(Int64, raw[8:(8+14)], base = 2)
+        #println(raw[8:(8+14)])
+        #println(raw)
+        #println(len)
+        subpacks = raw[(8+15):(8+15+len)]
+        ind = (8 + 15)
+        println(subpacks)
+        while true
+            r = packageParse(subpacks)
+            if r == -1
+                break
+            end
+            println(r)
+            push!(res, r[1])
+            println(res)
+            println(length(subpacks))
+            subpacks = subpacks[(r[2]):length(subpacks)]
+            println(length(subpacks))
+            println(subpacks)
+            ind += r[2] - 1
+        end
+        println("out")
+    else
+        amount = parse(Int64, raw[8:(8+10)], base = 2)
+        subpacks = raw[(8+11):length(raw)]
+        ind = (8 + 11)
+        println(subpacks)
+        println(amount)
+        for i = 1:(amount)
+            r = packageParse(subpacks)
+            push!(res, r[1])
+            println(i)
+            subpacks = subpacks[(r[2]):length(subpacks)]
+            println(subpacks)
+            ind += r[2] - 1
+        end
+    end
+    return ((version, type, res, id), ind)
 end
 function packageParse(raw)
-    version = parse(Int64, raw[1:3], base=2)
-    type = parse(Int64, raw[4:6], base=2)
-    println(type)
-    if type == 4
-        return literalValue
+    if length(raw) < (7)
+        return -1
     end
+    version = parse(Int64, raw[1:3], base = 2)
+    type = parse(Int64, raw[4:6], base = 2)
+    println("type ", type)
+    println("version ", version)
+    if type == 4
+        return literalValue(raw)
+    else
+        return Operators(raw)
+    end
+end
+function sumPackage(packg)
+    s = packg[1]
+    if packg[2] == 4
+        return s
+    else
+        for i in packg[3]
+            s += sumPackage(i)
+        end
+    end
+    return s
+end
+function RunPackage(packg)
+    if packg[2] == 4
+        return packg[3]
+    else
+        o = packg[2]
+        if o == 0
+            s = 0
+            for i in packg[3]
+                s += RunPackage(i)
+            end
+            return s
+        elseif o == 1
+            s = 1
+            for i in packg[3]
+                s *= RunPackage(i)
+            end
+            return s
+        elseif o == 2
+            s = typemax(Int64)
+            for i in packg[3]
+                s = min(s, RunPackage(i))
+            end
+            return s
+        elseif o == 3
+            s = typemin(Int64)
+            for i in packg[3]
+                s = max(s, RunPackage(i))
+            end
+            return s
+        elseif o == 5
+            return convert(Int64, RunPackage(packg[3][1]) > RunPackage(packg[3][2]))
+        elseif o == 6
+            return convert(Int64, RunPackage(packg[3][1]) < RunPackage(packg[3][2]))
+        elseif o == 7
+            return convert(Int64, RunPackage(packg[3][1]) == RunPackage(packg[3][2]))
+        end
+    end
+    return -10
 end
 function task1()
     a = readlines(ARGS[1])
@@ -46,6 +154,9 @@ function task1()
     for i in a[1]
         b *= conv[string(i)]
     end
-    println(packageParse(b))
+    println(b)
+    packg = packageParse(b)[1]
+    println(sumPackage(packg))
+    println(RunPackage(packg))
 end
 task1()
--- Day21/task1.jl
+++ Day21/task1.jl
@@ -1,8 +1,10 @@
 function move(pos, dice)
-    return (pos + dice) % 11
+    c = (pos - 1 + dice) % 10
+    return c + 1
 end
 mutable struct DeterMinisticStruct
     prev::Int64
+    times::Int64
 end
 function Dice(diceStruct)
     d = (diceStruct.prev + 1) % 101
@@ -10,24 +12,62 @@ function Dice(diceStruct)
         d = 1
     end
     diceStruct.prev = d
+    diceStruct.times += 1
     return d
+end
+d = Dict()
+function otherplayer(player)
+    if player == 1
+        return 2
+    else
+        return 1
+    end
+end
+function recursiveDec(player, points, pos)
+    if haskey(d, (player, points, pos))
+        return d[(player, points, pos)]
+    end
+    if maximum(points) >= 21
+        if points[1] >= 21
+            return [1, 0]
+        else
+            return [0, 1]
+        end
+    end
+    ret = []
+    for i = 1:3
+        for j = 1:3
+            for k = 1:3
+                pn = copy(pos)
+                pn[player] = move(pos[player], i + j + k)
+                pnts = copy(points)
+                pnts[player] += pn[player]
+                push!(ret, recursiveDec(otherplayer(player), pnts, pn))
+            end
+        end
+    end
+    f = sum(i[1] for i in ret)
+    g = sum(i[2] for i in ret)
+    d[(player, points, pos)] = [f, g]
+    return [f, g]
 end
 function simulate(players, dice, dicestruct)
     did = 0
     exitFlag = false
     while !exitFlag
-        for i in 1:length(players)
-            players[i][2] = move(players[i][2], dice(dicestruct)+dice(dicestruct)+dice(dicestruct))
+        for i = 1:length(players)
+            players[i][2] = move(players[i][2], dice(dicestruct) + dice(dicestruct) + dice(dicestruct))
             players[i][3] += players[i][2]
             if players[i][3] >= 1000
                 exitFlag = true
                 break
             end
             did += 1
+            #println(dicestruct.prev)
         end
-        println(players)
+        #println(players)
     end
-    println(players)
+    return minimum([i[3] for i in players])
 end
 function task1()
     inpreg = r"Player ([0-9]*) starting position: ([0-9]*)"
@@ -36,8 +76,10 @@ function task1()
         m = match(inpreg, line)
         push!(players, [parse(Int64, m[1]), parse(Int64, m[2]), 0])
     end
-    println(players)
-    dic = DeterMinisticStruct(0)
-    simulate(players, Dice, dic)
+    #println(players)
+    dic = DeterMinisticStruct(0, 0)
+    #t = simulate(players, Dice, dic)
+    #println(t * dic.times)
+    println(maximum(recursiveDec(1, [0, 0], [players[1][2], players[2][2]])))
 end
 task1()
